7c67f067e9fcb509ec0517bbcc0f56d11e4d87f8
Kamil Kulak <kamil.kulak@uk.tesco.com>
2017-05-13 14:22:35 +0200

    List of all env variables on app startup

--- src/main/java/pl/edu/agh/azurevm/Application.java
+++ src/main/java/pl/edu/agh/azurevm/Application.java
@@ -3,13 +3,24 @@ package pl.edu.agh.azurevm;
 import com.microsoft.azure.eventprocessorhost.EventProcessorHost;
 import com.microsoft.azure.eventprocessorhost.EventProcessorOptions;
 
+import java.util.Map;
+
 public class Application {
 
     public static void main(String[] args) throws Exception {
+
+        listEnvVariables();
+
         final EventProcessorHost host = EventProcessorHostFactory.create();
         final EventProcessorOptions options = new EventProcessorOptions();
         options.setExceptionNotification(new ErrorNotificationHandler());
         host.registerEventProcessor(EventProcessor.class, options).get();
+    }
+
+    private static void listEnvVariables() {
+        for (Map.Entry<String, String> envVariable : System.getenv().entrySet()) {
+            System.out.println(String.format("%s = %s", envVariable.getKey(), envVariable.getValue()));
+        }
     }
 
 }
